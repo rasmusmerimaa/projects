69538890b12e8aab47a255b704f82fef0c73cf95
Rasmus Merimaa <rasmus.merimaa@wisercat.net>
2025-02-03 07:39:03 +0200

    Airflow working with Terraform

--- main.tf
+++ main.tf
@@ -6,22 +6,152 @@ terraform {
     }
   }
 }
- 
+
 provider "docker" {
-  host    = "npipe:////.//pipe//podman-machine-default"
+  host = "npipe:////./pipe/podman-machine-default"
 }
- 
-resource "docker_image" "nginx" {
-  name         = "nginx"
-  keep_locally = false
+
+##########################
+# Create a shared network
+##########################
+resource "docker_network" "airflow_network" {
+  name = "airflow_network"
 }
- 
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.image_id
-  name  = "tutorial"
- 
+
+##########################
+# Postgres Service
+##########################
+resource "docker_volume" "postgres_data" {
+  name = "postgres_db_volume"
+}
+
+resource "docker_container" "postgres" {
+  name  = "postgres"
+  image = "postgres:13"
+
+  env = [
+    "POSTGRES_USER=airflow",
+    "POSTGRES_PASSWORD=airflow",
+    "POSTGRES_DB=airflow",
+  ]
+
   ports {
-    internal = 80
+    internal = 5432
+    external = 5432
+  }
+
+  volumes {
+    container_path = "/var/lib/postgresql/data"
+    volume_name    = docker_volume.postgres_data.name
+  }
+
+  healthcheck {
+    test         = ["CMD", "pg_isready", "-U", "airflow"]
+    interval     = "10s"
+    timeout      = "10s"
+    retries      = 5
+    start_period = "5s"
+  }
+
+  networks_advanced {
+    name = docker_network.airflow_network.name
+  }
+
+  restart = "always"
+}
+
+##########################
+# Redis Service
+##########################
+resource "docker_container" "redis" {
+  name  = "redis"
+  image = "redis:7.2-bookworm"
+
+  healthcheck {
+    test         = ["CMD", "redis-cli", "ping"]
+    interval     = "10s"
+    timeout      = "10s"
+    retries      = 5
+    start_period = "30s"
+  }
+
+  networks_advanced {
+    name = docker_network.airflow_network.name
+  }
+
+  restart = "always"
+}
+
+##########################
+# Airflow Initialization Service
+##########################
+resource "docker_container" "airflow_init" {
+  depends_on = [docker_container.postgres, docker_container.redis]
+  name       = "airflow-init"
+  image      = "apache/airflow:2.10.4"
+
+  entrypoint = ["/bin/bash"]
+  command    = [
+    "-c",
+    "airflow db upgrade && airflow users create --username airflow --password airflow --firstname Airflow --lastname Admin --role Admin --email airflow@example.com"
+  ]
+
+  env = [
+    "AIRFLOW__CORE__EXECUTOR=CeleryExecutor",
+    "AIRFLOW__DATABASE__SQL_ALCHEMY_CONN=postgresql+psycopg2://airflow:airflow@postgres/airflow",
+    "AIRFLOW__CELERY__RESULT_BACKEND=db+postgresql://airflow:airflow@postgres/airflow",
+    "AIRFLOW__CELERY__BROKER_URL=redis://redis:6379/0",
+    "AIRFLOW__CORE__FERNET_KEY=some_random_key",  // Replace with a secure key in production
+    "AIRFLOW__CORE__LOAD_EXAMPLES=true",
+    "AIRFLOW_UID=50000"
+  ]
+
+  user = "0:0"
+
+  networks_advanced {
+    name = docker_network.airflow_network.name
+  }
+
+  restart = "no"
+}
+
+##########################
+# Airflow Webserver Service
+##########################
+resource "docker_container" "airflow_webserver" {
+  depends_on = [docker_container.airflow_init]
+  name       = "airflow-webserver"
+  image      = "apache/airflow:2.10.4"
+  command    = ["webserver"]
+
+  ports {
+    internal = 8080
     external = 8000
   }
+
+  env = [
+    "AIRFLOW__CORE__EXECUTOR=CeleryExecutor",
+    "AIRFLOW__DATABASE__SQL_ALCHEMY_CONN=postgresql+psycopg2://airflow:airflow@postgres/airflow",
+    "AIRFLOW__CELERY__RESULT_BACKEND=db+postgresql://airflow:airflow@postgres/airflow",
+    "AIRFLOW__CELERY__BROKER_URL=redis://redis:6379/0",
+    "AIRFLOW__CORE__FERNET_KEY=some_random_key",  // Replace with your secure key
+    "AIRFLOW__CORE__LOAD_EXAMPLES=true",
+    "AIRFLOW_UID=50000"
+  ]
+
+  user = "50000:0"
+
+  networks_advanced {
+    name = docker_network.airflow_network.name
+  }
+
+  healthcheck {
+    test         = ["CMD", "curl", "--fail", "http://localhost:8080/health"]
+    interval     = "30s"
+    timeout      = "10s"
+    retries      = 5
+    start_period = "30s"
+  }
+
+  restart = "always"
 }
